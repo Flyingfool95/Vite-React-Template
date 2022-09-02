437b85adcefc60cfbaf431b53da05425542bd19f
FlyingFool95 <Jhernehult@gmail.com>
2022-09-02 18:13:28 +0200

    installed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <h1>This is my Vite template for React</h1>
-      <h2>This is my Vite template for React</h2>
-      <h3>This is my Vite template for React</h3>
-      <h4>This is my Vite template for React</h4>
-      <p>This is my Vite template for React</p>
+      <h1>Template Vite React w/ Typescript</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
         consequuntur mollitia laboriosam maxime, alias eveniet ab culpa eos
@@ -16,21 +12,6 @@ function App() {
         corporis consequatur rem obcaecati fugit, nesciunt quod facere minus,
         eos animi ratione quis sequi.
       </p>
-
-      <form>
-        <div>
-          <label>I am a label</label>
-          <input type="text" name="" id="" placeholder="Name" />
-          <input type="tel" name="" id="" placeholder="Phone" />
-        </div>
-        <div>
-          <input type="checkbox" name="" id="" />
-          <input type="number" name="" id="" placeholder="Number" />
-          <input type="email" name="" id="" placeholder="Email" />
-        </div>
-        <textarea name="" id="" placeholder="Message..." />
-        <input type="button" value="Button" />
-      </form>
     </div>
   );
 }
