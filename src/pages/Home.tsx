46eb8972e0202497aa7a1fe9815591f5c3af4f47
FlyingFool95 <jhernehult@gmail.com>
2022-12-29 21:46:44 +0100

    fixed modal to hav opening/closing animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,6 +10,8 @@ export default function Home() {
           <h2>Hello</h2>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis alias autem vitae?</p>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum tempore beatae doloribus nemo tempora tenetur placeat odio? Neque, eos laudantium.</p>
+
+          <img src="https://images.unsplash.com/photo-1672106157109-056c567e3833?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1088&q=80" alt="img" />
         </div>
       </Modal>
     </div>
