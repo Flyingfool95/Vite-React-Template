c8cc8906362e2ce7767de9e803cb4726e7e273ce
FlyingFool95 <jhernehult@gmail.com>
2023-01-01 19:46:53 +0100

    added simple slider component

---
+++ src/components/reusable/slider/Slider.tsx
@@ -0,0 +1,18 @@
+import "./Slider.scss"
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+
+export default function Slider() {
+  const [ref] = useKeenSlider<HTMLDivElement>();
+  return (
+    <div ref={ref} className="keen-slider">
+      <div className="keen-slider__slide number-slide1">
+        <img src="https://images.unsplash.com/photo-1672501426705-072bdfcf358c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1732&q=80" alt="img 1" />
+      </div>
+      <div className="keen-slider__slide number-slide2">
+        <img src="https://images.unsplash.com/photo-1671438799298-3fdc6c0f5f03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=778&q=80" alt="img 2" />
+
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Slider from "../components/reusable/slider/Slider";
 
 export default function Home() {
 
@@ -5,6 +6,9 @@ export default function Home() {
     <div>
       <h1>Template Vite React w/ Typescript</h1>
 
+      <div style={{ maxWidth: "720px", margin: "auto" }}>
+        <Slider />
+      </div>
 
     </div>
   );
