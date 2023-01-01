6126da3b0b99c0b6de0938058d72e2a221bd10ba
FlyingFool95 <jhernehult@gmail.com>
2023-01-01 19:47:29 +0100

    cleaned up home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1>Template Vite React w/ Typescript</h1>
-
-      <div style={{ maxWidth: "720px", margin: "auto" }}>
-        <Slider />
-      </div>
-
+      
     </div>
   );
 }
