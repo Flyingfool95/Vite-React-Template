009ed0a8f23f021ec9848e5e93d08389c876ec1a
FlyingFool95 <jhernehult@gmail.com>
2023-01-02 19:29:40 +0100

    added component - contact form

---
+++ src/components/forms/contact/Contact.tsx
@@ -0,0 +1,39 @@
+import "./Contact.scss";
+
+/* Component - email form */
+export default function Contact() {
+
+  //Functions
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    //Input values
+    const email = e.target[0].value;
+    const name = e.target[1].value;
+    const message = e.target[2].value;
+
+    console.log(`Sending email to: ${email} - ${name}. Saying: ${message}`);
+  }
+
+  //JSX
+  return (
+    <form className="contact-form" onSubmit={(e) => sendEmail(e)}>
+      <label>
+        Email
+        <input type="email" name="email" id="email" placeholder="Email" />
+      </label>
+      <label>
+        Name
+        <input type="text" name="name" id="name" placeholder="Name" />
+      </label>
+      <label>
+        Message
+        <textarea name="message" id="message" placeholder="Message" />
+      </label>
+
+      <label>
+        <input type="submit" value="Send" />
+      </label>
+    </form>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
+import Contact from "../components/forms/contact/Contact";
 
 export default function Home() {
 
   return (
     <div>
       <h1>Template Vite React w/ Typescript</h1>
-      
+      <Contact />
     </div>
   );
 }
