febc74ca5f0c46b3172cf3d87e313ed2c6d0548d
FlyingFool95 <jhernehult@gmail.com>
2023-01-04 17:30:25 +0100

    cleaned up home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,8 @@
-import Hero from "../components/hero/Hero";
-
-
 export default function Home() {
   return (
     <div>
       <h1>Template Vite React w/ Typescript</h1>
-      <Hero >
-        <h2>Test</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos voluptatibus corporis autem dolores ullam quae veniam nesciunt iure tempora amet?</p>
-      </Hero>
+
     </div>
   );
 }
